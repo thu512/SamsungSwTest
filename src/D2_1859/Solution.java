235ac3de5df5e542812d5b571a0b8665b3986d5e
changjoo <thu512@naver.com>
2017-10-13 23:18:23 +0900

    first

--- src/D2_1859/Solution.java
+++ src/D2_1859/Solution.java
@@ -6,8 +6,7 @@ import java.util.Scanner;
 class Solution {
 	static int result=0;
 	public static void main(String args[]) throws Exception	{
-		
-		
+
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		
